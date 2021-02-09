19aec23d580ff8f62b39ac0314e1c8c1d62f59cd
Xavi Aracil <xaracil@uoc.edu>
2021-02-09 17:06:28 +0100

    Custom DefaultJwtBuilder, setting KMSSigner as the signed of the JWT

---
+++ src/main/java/edu/uoc/lti/jwt/kms/JwtKMSBuilder.java
@@ -0,0 +1,50 @@
+package edu.uoc.lti.jwt.kms;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.JwtSigner;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Universitat Oberta de Catalunya
+ * Made for the project lti-13-jwt
+ */
+public class JwtKMSBuilder extends io.jsonwebtoken.impl.DefaultJwtBuilder {
+	private final String arn;
+	private final String region;
+
+	public JwtKMSBuilder(String arn, String region) {
+		this.arn = arn;
+		this.region = region;
+		this.signWith(fakePrivateKey(), SignatureAlgorithm.RS256);
+	}
+
+
+	@Override
+	protected JwtSigner createSigner(SignatureAlgorithm alg, Key key) {
+		return new KMSSigner(arn, region);
+	}
+
+	private Key fakePrivateKey() {
+		KeyPairGenerator kpg = null;
+		try {
+			kpg = KeyPairGenerator
+							.getInstance("RSA");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+
+		// initializing with 2048
+		kpg.initialize(2048);
+
+		// getting key pairs
+		// using generateKeyPair() method
+		KeyPair kp = kpg.genKeyPair();
+
+		// getting public key
+		return kp.getPrivate();
+	}
+}
