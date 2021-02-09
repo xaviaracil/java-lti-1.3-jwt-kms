e7dd95accc3dcab1513e7c1c7ae30e6afe5ed53e
Xavi Aracil <xaracil@uoc.edu>
2021-02-09 17:06:39 +0100

    Use custom DefaultJwtBuilder

--- src/main/java/edu/uoc/lti/jwt/deeplink/JWSTokenBuilder.java
+++ src/main/java/edu/uoc/lti/jwt/deeplink/JWSTokenBuilder.java
@@ -3,8 +3,8 @@ package edu.uoc.lti.jwt.deeplink;
 import edu.uoc.lti.claims.ClaimsEnum;
 import edu.uoc.lti.deeplink.DeepLinkingResponse;
 import edu.uoc.lti.deeplink.DeepLinkingTokenBuilder;
-import edu.uoc.lti.jwt.AlgorithmFactory;
 import edu.uoc.lti.ResponseMessageTypeEnum;
+import edu.uoc.lti.jwt.kms.JwtKMSBuilder;
 import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import lombok.RequiredArgsConstructor;
@@ -19,19 +19,17 @@ public class JWSTokenBuilder implements DeepLinkingTokenBuilder {
 	private final static long _5_MINUTES = 5 * 30 * 1000;
 	private final static String AUTHORIZED_PART = "azp";
 
-	private final String publicKey;
-	private final String privateKey;
+	private final String keyArn;
+	private final String region;
 
 	@Override
 	public String build(DeepLinkingResponse deepLinkingResponse) {
-		AlgorithmFactory algorithmFactory = new AlgorithmFactory(publicKey, privateKey);
 
-		final JwtBuilder builder = Jwts.builder()
+		final JwtBuilder builder = new JwtKMSBuilder(keyArn, region)
 						.setIssuer(deepLinkingResponse.getClientId())
 						.setAudience(deepLinkingResponse.getPlatformName())
 						.setIssuedAt(new Date())
 						.setExpiration(new Date(System.currentTimeMillis() + _5_MINUTES))
-						.signWith(algorithmFactory.getPrivateKey())
 						.claim(ClaimsEnum.MESSAGE_TYPE.getName(), ResponseMessageTypeEnum.LtiDeepLinkingResponse.name())
 						.claim(ClaimsEnum.VERSION.getName(), "1.3.0")
 						.claim(ClaimsEnum.DEPLOYMENT_ID.getName(), deepLinkingResponse.getDeploymentId())
